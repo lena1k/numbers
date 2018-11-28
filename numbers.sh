4b447b3785b9326e73f3390470bf68d9abec763a
Lena Khalidi <khalidi@chapman.edu>
2018-11-28 10:33:53 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# numbers.sh
+
+# Lena Khalidi
+
+echo "Enter a positive number":
+
+read number
+
+N =1
+
+while [ $N -le $number ]
+
+do
+	if [  $((N%2)) -eq 0 ]
+	then
+		echo $N "even"
+		N=$[$N+1]
+
+	else
+		echo $N "odd"
+		N=$[N+1]
+	fi
+done
+
+
+												
